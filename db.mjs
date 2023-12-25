29880c8102213679c538eac66d91b39755936400
Amritpal Singh <55401139+iamamritpalrandhawa@users.noreply.github.com>
2023-12-25 23:24:59 +0530

    Update db.mjs

--- db.mjs
+++ db.mjs
@@ -3,13 +3,14 @@ import mongoose from 'mongoose';
 // const mongoURL = 'mongodb://127.0.0.1:27017/MusicApp';
 const mongoURL = 'mongodb+srv://chatapp0321:tU7jEziHy0QzfytE@musicapp.isql17r.mongodb.net/MusicApp?retryWrites=true&w=majority';
 
-// const connectionParams = {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true
-// }
+const connectionParams = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}
 const connectToMongo = async () => {
     try {
-        await mongoose.connect(mongoURL);
+        await mongoose.connect(mongoURL, connectionParams);
+
         console.log('Connection made successfully');
     } catch (error) {
         console.error('Connection error:', error);
